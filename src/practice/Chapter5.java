a5d96785826a48bd42c821828b689c5a167e15b2
yusuke-0121 <pj2j9999@gmail.com>
2023-08-06 06:11:34 +0900

    課題提出

---
+++ src/practice/Chapter5.java
@@ -0,0 +1,25 @@
+package practice;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Chapter5 {
+    public static void main(String[] args) {
+        List<Task> tasks = new ArrayList<>();
+
+        tasks.add(new Task(LocalDate.of(2021, 10, 21), "牛乳を買う"));
+        tasks.add(new Task(LocalDate.of(2021, 9, 15), "○○社面接"));
+        tasks.add(new Task(LocalDate.of(2021, 12, 4), "手帳を買う"));
+        tasks.add(new Task(LocalDate.of(2021, 8, 10), "散髪に行く"));
+        tasks.add(new Task(LocalDate.of(2021, 11, 9), "スクールの課題を解く"));
+
+        Collections.sort(tasks);
+
+        for (Task task : tasks) {
+            System.out.println(task);
+        }
+    }
+}
+
---
+++ src/practice/Task.java
@@ -0,0 +1,23 @@
+package practice;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class Task implements Comparable<Task> {
+    private LocalDate date;
+    private String task;
+
+    public Task(LocalDate date, String task) {
+        this.date = date;
+        this.task = task;
+    }
+
+    public int compareTo(Task other) {
+        return date.compareTo(other.date);
+    }
+
+    public String toString() {
+        DateTimeFormatter f = DateTimeFormatter.ofPattern("yyyy年M月d日");
+        return date.format(f) + ":" + task;
+    }
+}
